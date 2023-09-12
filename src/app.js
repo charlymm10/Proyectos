0b8c3ada98ed7269ad7ea2b03a892c359cd7aed9
charlymm10 <mayorgapiedrajuan@gmail.com>
2023-09-12 15:53:56 -0600

    agregamos conexion de bd, creamos base de datos

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require('express');//referencia a framework express
 const app = express();//se crea la aplicacion de express
 const log = require('morgan');//para saber los clientes conectados
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const IndexRoutes = require('./routers/index.js');
+const {default: mongoose} = require('mongoose');
 
 
 app.set('port', process.env.PORT || 3000);//asigna un puerto
@@ -14,9 +16,20 @@ app.use(bodyParser.urlencoded({extended:false}));
 //rutas
 app.use('/', IndexRoutes);
 
-app.liste(app.get('port'), () =>{
+app.listen(app.get('port'), () =>{
     console.log('El servidor esta funcionando en el puerto ', app.get('port'));
 }
 
 );
+//conectar a la base de datos
+mongoose.connect("mongodb+srv://charly:JuanCarlos2001@cluster0.fxftri0.mongodb.net/Imprenta?retryWrites=true&w=majority")
+.then(bd=>console.log('BD conectada!'))
+.catch(err=>console.log(err));
+
+
+//establecer sistema de vista
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+
 //desplegar mensaje de conectado
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const router = express.Router();
+const model = require('../models/Productos.js')();
+const Productos = require('../model/Productos.js');
 
-const router = express.Route();
+router.get('/', async (req,res)=>{
+    const datos = await Productos.find();
+    console.log(datos);
+    res.render('index.ejs',{
+        datos
+    });
+});
+
 
 module.exports = router;
